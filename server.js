6362749407e4d2cf4898b6cd983eada6a15a534c
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-10-17 03:33:23 -0400

    add dist

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/'));
+app.use('/assets', express.static(__dirname + '/src/assets/'));
+
+app.listen(process.env.PORT || 8080);
